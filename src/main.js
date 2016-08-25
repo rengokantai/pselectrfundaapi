aa16de7503a9ce030dab19cb60040163756b70d6
Hernan Y.Ke <yuriqiao@gmail.com>
2016-08-24 23:51:11 -0400

    Add menu

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ const electron = require('electron');
 const path = require('path');
 const app = electron.app;
 const Tray = electron.Tray
+const Menu = electron.Menu
 app.on('ready',_=>{
-	new Tray(path.join('src','icon.png'))
+	const tray = new Tray(path.join('src','icon.png'))
+	const contextMenu = Menu.buildFromTemplate([{
+		label:'a',
+		click: _=>console.log('test')
+	}])
+	tray.setContextMenu(contextMenu)
 })
