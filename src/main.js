e61e9694a60b61c13f3e548ab07292a77e1eed74
Hernan Y.Ke <yuriqiao@gmail.com>
2016-09-17 23:42:31 -0400

    clipboard first try

--- src/main.js
+++ src/main.js
@@ -1,23 +1,33 @@
 const electron = require('electron');
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow
-const Menu = electron.Menu
+const path = require('path');
+
+const {app,clipboard,Menu, Tray}=electron;
+const STACK_SIZE=3;
+
+function addToStack(item,stack){
+	return [item].concat(stack.length>=STACK_SIZE?stack.slice(0,stack.length-1):stack)
+}
+
+function checkClipChange(clipboard,onChange){
+	let cache = clipboard.readText();
+	let latest
+	setInterval(_=>{
+		latest = clipboard.readText()
+		if(latest!==cache){
+			cache=latest
+			onChange(cache)
+		}
+	},1000)
+}
+
 app.on('ready',_=>{
-	new BrowserWindow()
-	const name = electron.app.getName();
-	const template = [{
-		label:name,
-		submenu:[{
-			label:`About ${name}`,
-			role:'about'
-		},{
-			type:'separator'
-		},{
-			label:'Quit',
-			click:_=>{app.quit()},
-				accelerator: 'CmdOrCtrl+q'
-		}]
-	}]
-	const menu = Menu.buildFromTemplate(template);
-	Menu.setApplicationMenu(menu);
+	let stack=[];
+	const tray = new Tray(path.join('src','trayIcon.png'))
+	tray.setContextMenu(Menu.buildFromTemplate([{
+		label:'<Empty>',enabled:false
+	}]))
+	checkClipChange(clipboard,text=>{
+		stack = addToStack(text,stack)
+		console.log(stack);
+	})
 })
